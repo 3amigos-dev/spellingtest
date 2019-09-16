303844a17a3d87dbd4dc883e38544323c565d00b
Tim Gates <tim.gates@iress.com>
2019-09-17 07:27:21 +1000

    Reset hooks

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,10 +6,5 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$(dirname "${BASEDIR}")"
 
 cd "${TOP}"
-readarray -t FILES < <(git status -s docs | grep -E ^.D | sed s/^...//)
-if [ ${#FILES[@]} -ne 0 ] ; then
-    git checkout "${FILES[@]}"
-fi
 git checkout -- \
- docs/index.rst \
- logo.png
+ app/pip/3.4/app/requirements.txt
